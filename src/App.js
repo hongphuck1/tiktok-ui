3504839d11095d33599d1bf51a4ac0f8652c5f60
hongphuck1 <duonghongphuclsss@gmail.com>
2023-01-04 06:00:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import "./App.css";
-
+import "~/App.css";
 function App() {
-  return <div className="App">Xin chào</div>;
+  return <div className="App">Xin chào các bạn</div>;
 }
 
 export default App;
