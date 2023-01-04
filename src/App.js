6a78f72f8b30311dbb46862c8465e6bf0a9bf4a9
hongphuck1 <duonghongphuclsss@gmail.com>
2023-01-05 01:01:01 +0700

    Cấu Hình Router/Layout website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import "~/App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRouters } from '~/router';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 function App() {
-  return <div className="App">Xin chào các bạn</div>;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRouters.map((route, index) => {
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        const Page = route.components;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Content from '~/pages/Content';
+import Upload from '~/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
+import Search from '~/pages/Search';
+
+// Công khai pages
+const publicRouters = [
+    {
+        path: '/',
+        components: Home,
+    },
+    {
+        path: '/following',
+        components: Following,
+    },
+    {
+        path: '/content',
+        components: Content,
+    },
+    {
+        path: '/upload',
+        components: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        components: Search,
+        layout: null,
+    },
+];
+
+// Đóng kín
+const privateRouters = [];
+
+export { publicRouters, privateRouters };
