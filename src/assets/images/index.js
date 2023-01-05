9026abcca2f937a25893bbf3d111dd9912feefeb
hongphuck1 <duonghongphuclsss@gmail.com>
2023-01-06 02:08:51 +0700

    Xây dựng UI phần Header #1

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    // logoTiktokPng: require('~/assets/images/TikTok_logo.svg.png'),
+};
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import classnames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
-const cx = classnames.bind(styles);
+import images from '~/assets/images';
 
-// import LogoTiktok from '~/image/TikTok_logo.svg.png';
+const cx = classnames.bind(styles);
 
 const Header = () => {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* logo */}
-                {/* <img src={LogoTiktok}></img> */}
-                <div style={{ textAlign: 'center' }}>Header</div>
+                <div className={cx('logo')}>{<img src={images.logo} alt="tiktok"></img>}</div>
                 {/* search */}
+                <div className={cx('search')}>
+                    <input
+                        className={cx('search-input')}
+                        type="text"
+                        placeholder="Search accounts and videos"
+                        spellCheck="false"
+                    />
+                    <button className={cx('clear-btn')}>
+                        <FontAwesomeIcon className={cx('close-icon')} icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading-icon')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon className={cx('search-icon')} icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                {/* actions */}
+                <div className={cx('actions')}>content</div>
             </div>
         </div>
     );
