3f8fa8a33d039c0f4d336113bdab6f11924b9f48
hongphuck1 <duonghongphuclsss@gmail.com>
2023-01-05 17:01:07 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '~/components/Layout/components/Header';
+import classnames from 'classnames/bind';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+const cx = classnames.bind(styles);
 
 const DefaultLayout = ({ children }) => {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './Header.module.scss';
+const cx = classnames.bind(styles);
+
+// import LogoTiktok from '~/image/TikTok_logo.svg.png';
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* logo */}
+                {/* <img src={LogoTiktok}></img> */}
+                <div style={{ textAlign: 'center' }}>Header</div>
+                {/* search */}
+            </div>
+        </div>
+    );
 };
 
 export default Header;
